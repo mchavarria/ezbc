a094ab1cfad1e9a996c9046251c08be415ffcd5f
Chavarria Miguel Angel <migue.chavarria@gmail.com>
2018-09-09 01:42:12 -0300

    Backend design implemented

---
+++ src/AppBundle/Controller/BackendController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class BackendController
+ * @Route("/app")
+ */
+class BackendController extends Controller
+{
+    /**
+     * @Route("/index", name="backend_dashboard")
+     *
+     * @Template("@App/Backend/index.html.twig")
+     *
+     * @return array
+     */
+    public function indexAction()
+    {
+        return [];
+    }
+}
